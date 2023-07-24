f8ad5a13d5203d215e9a43c14fca76379a377c46
lukef7fywmrp <lukef7fywmrp@gmail.com>
2023-07-24 18:23:29 +0400

    Add loaders, date-fns, and lightweight-charts dependencies

--- typings.d.ts
+++ typings.d.ts
@@ -12,6 +12,44 @@ type Ticker = [
   low: number
 ];
 
+type TickerInfo = [
+  bid: number,
+  bidSize: number,
+  ask: number,
+  askSize: number,
+  dailyChange: number,
+  dailyChangeRelative: number,
+  lastPrice: number,
+  volume: number,
+  high: number,
+  low: number
+];
+
 type TradingPair = string;
 
 type Trade = [id: number, mts: number, amount: number, price: number];
+
+type Section = "hist" | "last";
+
+type TimeFrame =
+  | "1m"
+  | "5m"
+  | "15m"
+  | "30m"
+  | "1h"
+  | "3h"
+  | "6h"
+  | "12h"
+  | "1D"
+  | "1W"
+  | "14D"
+  | "1M";
+
+type Candle = [
+  mts: number,
+  open: number,
+  close: number,
+  high: number,
+  low: number,
+  volume: number
+];
